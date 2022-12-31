75ef3567b7696a74980eb6e433e6212a9aa2af02
Saurav903 <sk8517167@gmail.com>
2022-12-31 14:48:01 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css"
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
     <div className="App">
-      hello world
+      <Navbar/>
+      <div className="name">
+        <h1>SAURAV KUMAR</h1>
+        <h3 className="h3top">Full Stack Web Developer</h3>
+      </div>
     </div>
+      <div className="topmain">
+      <div className="avatar">
+        <img src="https://i.imgur.com/9u4Iskv.jpg" alt="" width="100%"/>
+      </div>
+      <div className="avatar_title">
+        <p>
+        I am a Full stack web developer doing militry style coding and learning MERN Stack at Masai School. I have done various group as well as individual projects in my coding journey. I also did my B.Tech in Automobile Engineering from MCKVIE.
+        </p>
+      </div></div>
+    </>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { FaGg } from "react-icons/fa";
+
+const Links = ['Home', 'About Me', 'Skills',"Projects","Contact","Resume"];
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
+
+export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box position="fixed" zIndex={1} bg={useColorModeValue('gray.100', 'gray.900')} px={4} w="100%">
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          
+          <Flex alignItems={'center'}>
+                <FaGg
+                  size="30px"
+                />
+          </Flex>
+          <HStack spacing={8} alignItems={'center'}>
+            {/* <Box>Logo</Box> */}
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }} fontSize="20px">
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+      
+    </>
+  );
+}

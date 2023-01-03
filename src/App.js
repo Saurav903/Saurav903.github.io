71ef12f535187b67fb08ccf5ef7aef13f5b69b9a
Saurav903 <sk8517167@gmail.com>
2023-01-03 16:29:10 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
 import "./App.css"
 import Navbar from './components/Navbar';
+import { Particles1 } from "./components/Particles";
+import { TechSkill } from "./components/TechSkill";
+
 
 function App() {
+  
   return (
     <>
     <div className="App">
+    
       <Navbar/>
+      
       <div className="name">
         <h1>SAURAV KUMAR</h1>
         <h3 className="h3top">Full Stack Web Developer</h3>
@@ -20,7 +26,14 @@ function App() {
         <p>
         I am a Full stack web developer doing militry style coding and learning MERN Stack at Masai School. I have done various group as well as individual projects in my coding journey. I also did my B.Tech in Automobile Engineering from MCKVIE.
         </p>
-      </div></div>
+      </div>
+      <div id="tsparticles">
+      <Particles1 />
+      </div>
+      <div>
+        <TechSkill/>
+      </div>
+      </div>
     </>
   );
 }
---
+++ src/components/TechSkill.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Heading } from '@chakra-ui/react'
+import "./techskill.css" 
+export const TechSkill = () => {
+  return (
+    <div className='tech_main'>
+        
+        <Heading textAlign="center" fontFamily="monospace">Technical Skills</Heading>
+    </div>
+  )
+}
